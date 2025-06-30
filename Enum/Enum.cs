04e10252e9ce6941c658846b28048ce7e61eaccc
mihneazamfir <mihneazamfir04@gmail.com>
2025-06-30 12:05:08 +0300

    getBands getBookings createBand

--- Enum/Enum.cs
+++ Enum/Enum.cs
@@ -1,10 +1,10 @@
 public enum Genre
 {
-    Rock,
-    Pop,
-    HipHop,
-    Country,
-    Reggae,
-    Techno,
-    House,
+    Rock = 0,
+    Pop = 1,
+    HipHop = 2,
+    Country = 3,
+    Reggae = 4,
+    Techno = 5,
+    House = 6
 }
